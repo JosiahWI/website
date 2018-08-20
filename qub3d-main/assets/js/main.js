e2f1d5e112fa5547ae8c666ce23f5f5abb79b60c
Vyom Fadia <vyom.fadia@outlook.com>
2018-08-20 12:17:23 +0100

    Add smooth-scrolling
    Summary: Add smooth-scrolling to the site
    
    Test Plan: N/A
    
    Reviewers: #trusted, blaclegi, TMcSquared
    
    Reviewed By: #trusted, blaclegi
    
    Differential Revision: https://phab.qub3d.tk/D187

--- qub3d-main/assets/js/main.js
+++ qub3d-main/assets/js/main.js
@@ -1,3 +1,8 @@
+// ----------------------------------------------------------
+// ------------------- SLIDE IN ON SCROLL -------------------
+//-----------------------------------------------------------
+
+
 (function ($) {
 
     /**
@@ -60,3 +65,44 @@ win.scroll(function(event) {
   });
   
 });
+
+// ----------------------------------------------------------
+// -------------------- SMOOTH SCROLLING --------------------
+//-----------------------------------------------------------
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
